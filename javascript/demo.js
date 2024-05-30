75529a032e99012c92716e972bd79c58b206ffc6
shanita <shanitakalamkar11@gmail.com>
2024-05-30 17:18:12 +0530

    first commit

---
+++ javascript/demo.js
@@ -0,0 +1,55 @@
+// document.getElementById("demo").innerHTML=
+// 5+5;
+
+// document.write(5+6);
+// document.write("Hello...");
+// window.alert("welcome");
+// console.log("Welcome in pune");
+// var x=5;
+// y=6;
+// z=x+y;
+// console.log(z);
+
+// document.getElementById("demo1").innerHTML=                 "Hello" 
+//  + " " + "world";
+
+
+//  var a;
+//  "5"===5;
+
+// let b=5;
+// let c=6;
+// let d=b+c;
+// console.log(d)
+
+
+// const b=4;
+// const c=6;
+// const d=b+b;
+// console.log(b);
+
+// var f;
+// f=6;
+// {
+// var f=10;
+// }
+// console.log(f);
+// try{
+// car="Swift";
+// document.write(car);
+// let  car;
+// }
+// catch(err){
+// document.write(err);
+
+// }
+
+
+try{
+    const PI=3.14 ;
+    PI=3.14;
+}
+catch(err){
+    document.write(err);
+}
+ 
